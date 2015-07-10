3d7de7d6118830e0906ddec134619085b8edd6a1
Takayuki Fuwa <yue@eldoom.me>
2015-07-11 00:06:38 +0900

    Add DB class

---
+++ classes/timeline/dao/log.php
@@ -0,0 +1,24 @@
+<?php
+namespace block_minerva\timeline\dao;
+
+class log
+{
+    private $context;
+
+    function __construct($context)
+    {
+        $this->context = $context;
+    }
+
+    public function myself($page)
+    {
+        global $DB;
+
+
+    }
+
+    public function in_course($courseid)
+    {
+
+    }
+}
