1cac5c6af64ad69ce055f2b0e101526e84d3ffc1
yuesan <yae.yue@gmail.com>
2017-09-16 17:42:42 +0900

    Add Capability "myaddinstance"

--- db/access.php
+++ db/access.php
@@ -35,4 +35,14 @@ $capabilities = array(
         ),
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
+    'block/minerva:myaddinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
 );
